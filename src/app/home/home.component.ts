b05244015d2baf6c64383f3c42425c64155fb324
shubham Shrivastava <ss080994@gmail.com>
2018-01-14 21:18:57 +0530

    deletion change

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -52,5 +52,7 @@ export class HomeComponent implements OnInit {
   removeItem(i){
     this.goals.splice(i,1);
     this._data.changeGoal(this.goals);
+    this.itemCount=this.goals.length;
+    
   }
 }
